39bbc3b77af0031638b5247b8bb6565f54eb20ea
Stefan Wapnick <stefanwapnick@hotmail.com>
2015-12-20 15:16:06 -0500

    removed bin from linked dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :rbenv_path, '/home/deploy/.rbenv/'
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
