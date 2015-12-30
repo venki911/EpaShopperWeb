849fd1685a7f518a8700292d6b01e039952434c4
Stefan Wapnick <stefanwapnick@hotmail.com>
2015-12-30 10:19:57 -0500

    Changed source email to environment variable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'localhost' }
+  config.action_mailer.default_url_options = { :host => ENV['EPA_SERVER_URL'] }
   config.action_mailer.smtp_settings = {
       :address              => 'smtp.gmail.com',
       :port                 => 587,
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => '45.55.161.106' }
+  config.action_mailer.default_url_options = { :host => ENV['EPA_SERVER_URL'] }
   config.action_mailer.smtp_settings = {
       :address              => 'smtp.gmail.com',
       :port                 => 587,
